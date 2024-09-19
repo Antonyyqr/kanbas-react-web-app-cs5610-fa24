0c101a79bdbcd3d8d74041197fe062e6b5e64649
antonyyang <Antonyyqr5519@gmail.com>
2024-09-18 23:53:40 -0700

    A1 finished and updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+//import "./App.css";
 import Labs from "./Labs";
+import Kanbas from "./Kanbas";
+import { HashRouter, Link, Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <h1>Welcome to Web Dev</h1>
-      <Labs />
-    </div>
+    <HashRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="/Kanbas" />} />
+          <Route path="/Labs/*" element={<Labs />} />
+          <Route path="/Kanbas/*" element={<Kanbas />} />
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
 
